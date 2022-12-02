690b4259309589f7a2c07ae68f045d848ba27ca1
renanzdm <renanzdm@gmail.com>
2022-12-02 08:17:22 -0300

    fix errors text fields

--- empanturra/lib/src/commom_widgets/customize_text_field.dart
+++ empanturra/lib/src/commom_widgets/customize_text_field.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 
 class CustomizeTextFiled extends StatefulWidget {
-    CustomizeTextFiled({ Key? key, required this.hintText,
-      this.isDense = true,
-      this.hintColor = Colors.black87,
-      this.obscureText = false,
-      required this.textInputType,
-      this.isSecret = false,
-      this.isPasswordField = false,
-      required this.suffixIcon,
-      required this.controller,
-      required this.validator,
-      }) : super(key: key);
+  const CustomizeTextFiled({
+    Key? key,
+    required this.hintText,
+    this.isDense = true,
+    this.hintColor = Colors.black87,
+    this.obscureText = false,
+    required this.textInputType,
+    this.isSecret = false,
+    this.isPasswordField = false,
+    required this.suffixIcon,
+    required this.controller,
+    required this.validator,
+  }) : super(key: key);
 
-    final String hintText;
+  final String hintText;
   final bool isDense;
   final Color hintColor;
   final bool obscureText;
@@ -21,7 +23,7 @@ class CustomizeTextFiled extends StatefulWidget {
   final bool isSecret;
   final IconData suffixIcon;
   final bool isPasswordField;
-   final String? Function(String value) validator;
+  final String? Function(String? value) validator;
   final TextEditingController controller;
   @override
   State<CustomizeTextFiled> createState() => _CustomizeTextFiledState();
@@ -33,25 +35,30 @@ class _CustomizeTextFiledState extends State<CustomizeTextFiled> {
     return Padding(
       padding: const EdgeInsets.only(bottom: 12),
       child: TextFormField(
-        validator:widget.validator(value),
+        validator: widget.validator,
         controller: widget.controller,
-        keyboardType:widget.textInputType,
+        keyboardType: widget.textInputType,
         cursorColor: Colors.yellow[700],
         cursorRadius: const Radius.circular(10),
         style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-        obscureText:widget.obscureText,
+        obscureText: widget.obscureText,
         decoration: InputDecoration(
           hintText: widget.hintText,
           hintStyle: const TextStyle(fontWeight: FontWeight.w600, color: Colors.black87),
-          suffixIcon: Icon(widget.suffixIcon, color: Colors.black,),
-            enabledBorder:   UnderlineInputBorder(borderSide: BorderSide(color: Colors.grey.shade300,style: BorderStyle.solid),
-            ),
-            border: const UnderlineInputBorder(borderSide: BorderSide( color: Colors.yellow,)
-            ),
-            focusedBorder:const UnderlineInputBorder(borderSide: BorderSide(color: Colors.blue,style: BorderStyle.solid)),
-            ),
+          suffixIcon: Icon(
+            widget.suffixIcon,
+            color: Colors.black,
+          ),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey.shade300, style: BorderStyle.solid),
+          ),
+          border: const UnderlineInputBorder(
+              borderSide: BorderSide(
+            color: Colors.yellow,
+          )),
+          focusedBorder: const UnderlineInputBorder(borderSide: BorderSide(color: Colors.blue, style: BorderStyle.solid)),
+        ),
       ),
     );
   }
-
 }
